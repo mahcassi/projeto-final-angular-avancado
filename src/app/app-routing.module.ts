48cfbc159cf74df6a2c3d9993c2ecbbe12507c9b
Maria Eduarda <mariaed.cassiano@gmail.com>
2022-07-11 22:03:46 -0300

    feat - adicionado estrutura de cada componente de navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponet } from './navegacao/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponet }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
