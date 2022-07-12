eb08d7aefc2404f6e0e632603a7d763da5467feb
Maria Eduarda <mariaed.cassiano@gmail.com>
2022-07-12 18:11:06 -0300

    feat - lazy loading módulo conta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContaModule } from './conta/conta.module';
 import { HomeComponet } from './navegacao/home/home.component';
+import { NotFoundComponet } from './navegacao/not-found/not-found.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponet }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponet },
+  {
+    path: 'conta',
+    loadChildren: () => import('./conta/conta.module').then(m => m.ContaModule)
+  },
+  { path: '**', component: NotFoundComponet }
 ];
 
 @NgModule({
--- src/app/conta/conta.module.ts
+++ src/app/conta/conta.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 
-
+import { ContaRoutingModule } from './conta.route';
+import { ContaAppComponente } from './conta.app.component';
 
 @NgModule({
-  declarations: [CadastroComponent, LoginComponent],
+  declarations: [
+    ContaAppComponente,
+    CadastroComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class ContaModule { }
