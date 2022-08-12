0f3e67a042efbc61dce49596d5a6b637dda6de70
Maria Eduarda <mariaed.cassiano@gmail.com>
2022-08-11 22:11:43 -0300

    feat: adicionada estrutura html dos componentes do fornecedor e validações de campo

--- src/app/fornecedor/lista/lista.component.ts
+++ src/app/fornecedor/lista/lista.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FornecedorService } from '../services/fornecedor.service';
+import { Fornecedor } from '../models/fornecedor';
 
 @Component({
   selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  templateUrl: './lista.component.html'
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  public fornecedores: Fornecedor[];
+  errorMessage: string;
+
+  constructor(private fornecedorService: FornecedorService) { }
 
   ngOnInit(): void {
+    this.fornecedorService.obterTodos()
+      .subscribe(
+        fornecedores => this.fornecedores = fornecedores,
+        error => this.errorMessage);
   }
-
 }
