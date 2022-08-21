d2a83a7fe6695330f92ca947ef5cf9352c3d8009
Maria Eduarda <mariaed.cassiano@gmail.com>
2022-08-21 15:00:51 -0300

    feat: endereço do fornecedor

--- src/app/fornecedor/detalhes/detalhes.component.ts
+++ src/app/fornecedor/detalhes/detalhes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+
 import { Fornecedor } from '../models/fornecedor';
-import { FornecedorService } from '../services/fornecedor.service';
 
 @Component({
   selector: 'app-detalhes',
@@ -13,14 +14,18 @@ export class DetalhesComponent implements OnInit {
   fornecedor: Fornecedor = new Fornecedor();
 
   constructor(
-  private route: ActivatedRoute,
-  private fornecedorService: FornecedorService)
-  {
+    private route: ActivatedRoute) {
     this.fornecedor = this.route.snapshot.data['fornecedor'];
   }
 
 
   ngOnInit(): void {
+    console.log(this.EnderecoCompleto())
+  }
+
+  public EnderecoCompleto(): string {
+    return this.fornecedor.endereco.logradouro + ", " + this.fornecedor.endereco.numero + " - " + this.fornecedor.endereco.bairro +
+      ", " + this.fornecedor.endereco.cidade + " - " + this.fornecedor.endereco.estado;
   }
 
 }
